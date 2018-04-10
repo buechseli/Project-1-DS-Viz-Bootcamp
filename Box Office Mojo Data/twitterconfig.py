ca2c71dff67c097394af089e5dd9319286c41624
Grace Chan <gkchan@qualcomm.com>
2018-04-09 21:33:04 -0700

    adding twitter hashtags

---
+++ Box Office Mojo Data/twitterconfig.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = 'NV4AmQgzwJzWnBaSWa5unbbnR'
+consumer_secret = 'm4DOPZ7mos87XPljxZCBvnqXe29MDlKvo6wisuPN5R8r2cUBSx'
+access_token = '979170021992677376-sG0j4sW3J3pY9C95f1twdsQiRxmeioa'
+access_token_secret = 'rezwjR90ZDjf9m4bEjajVZUrbMjb5h0bLSNmmFKqlS76T'
